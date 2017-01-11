456b344a9f63abaa8cead6ec0550d0333503206e
swampape-Roth <rothvann2000@gmail.com>
2017-01-10 21:55:05 -0800

    Initial Commit
    Added basic structure of robot code. 
    
    Subsystems should extend Threaded, initialize RUNNINGSPEED to a number,
    and include a run() function. run() should not block because the task is
    not guaranteed to stop. However, it is fine if run() is blocking because
    the next execution of call is simply pushed back until the initial call
    finishes. run() should not be synchronized and instead should use
    functions that are synchronized or use synchronized blocks.
    
    MotorGroup is used like: MotorGroup<CANTalon> cantalons = new
    MotorGroup<CANTalon>(motor1, motor2, motor3, ... motorn); The
    constructor accepts an arbitrary number of arguments. Once initialized
    motors can be removed or added using: cantalons.remove(motor); or
    cantalons.add(motor); To have all motors in the group call a function
    use the call function like: cantalons.call(CANTalon::set, 0.5); That
    would be the equivalent of having motor1, motor2 ... motorn each call:
    motor1.set(0.5); motor2.set(0.5);
    
    CANTalonChecker is initialized with a max current. Motors can be added
    or removed using: CANTalonChecker mainChecker = new
    CANTalonChecker(maxcurrent); mainChecker.add(motor);
    mainChecker.remove(motor); This class only accepts CANTalons as other
    speedcontrollers do not have current checking. This class will be
    changed to prevent spiking from causing accidental disables.

---
+++ src/org/usfirst/frc/team3476/utility/CANTalonChecker.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team3476.utility;
+
+import java.util.ArrayList;
+
+import com.ctre.CANTalon;
+
+public class CANTalonChecker extends Threaded {
+
+	ArrayList<CANTalon> motorArray;
+
+	private int MAXCURRENT;
+	// not sure about max current
+
+	public CANTalonChecker(int MAX) {
+		RUNNINGSPEED = 250;
+		MAXCURRENT = MAX;
+	}
+
+	public void add(CANTalon motorIn) {
+		motorArray.add(motorIn);
+	}
+	
+	public void remove(CANTalon motorIn) {
+		motorArray.remove(motorIn);
+	}
+	@Override
+	public void run() {
+		for (CANTalon motor : motorArray) {
+			if (motor.getOutputCurrent() > MAXCURRENT) {
+				motor.disableControl();
+			}
+		}
+
+	}
+
+}
---
+++ src/org/usfirst/frc/team3476/utility/Threaded.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team3476.utility;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public abstract class Threaded implements Runnable {
+	private ScheduledFuture<?> taskFuture;
+
+	protected int RUNNINGSPEED;
+
+	@Override
+	public abstract void run();
+
+	public void startTask(ScheduledExecutorService execIn) {
+		if (RUNNINGSPEED <= 0) { // int is zeroinitialized
+			System.out.println("RUNNINGSPEED not initialized or is negative");
+		} else {
+			taskFuture = execIn.scheduleAtFixedRate(this, 0, RUNNINGSPEED, TimeUnit.MILLISECONDS);
+		}
+	}
+
+	public void endTask() {
+		if (taskFuture != null) {
+			if (!taskFuture.isCancelled()) {
+				taskFuture.cancel(false);
+			}
+		}
+	}
+}
