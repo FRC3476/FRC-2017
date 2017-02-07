3ced5c7f063eee73539675c6f823bc4226d185aa
swampApe-roth <rothvann2000@gmail.com>
2017-02-06 18:09:21 -0800

    Intake Revision
    Intake is now not threaded
    setState controls up/down motion
    setSucking controls intaking

--- src/org/usfirst/frc/team3476/subsystem/Intake.java
+++ src/org/usfirst/frc/team3476/subsystem/Intake.java
@@ -1,15 +1,13 @@
 package org.usfirst.frc.team3476.subsystem;
-import org.usfirst.frc.team3476.utility.Threaded;
 
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.TalonControlMode;
 
 import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.Talon;
 
 public class Intake {
 
-	enum CurrentState {UP,DOWN};
+	enum IntakeState {UP,DOWN};
 
 	public static int UP = 1; //replace with some positive number like 1
 	public static int DOWN = 0;//replace with some negative number like -1
@@ -19,50 +17,41 @@ public class Intake {
 	
 	CANTalon masterTalon, slaveTalon;
 	
-	private CurrentState now;
-	private CurrentState wanted;
-	private double setpoint;
-	
-	
-	public void setSetpoint(double setpoint){
-		this.setpoint = setpoint;
-		masterTalon.setSetpoint(setpoint);
-	}
-
+	private IntakeState currentState;
 
 	private Intake (int Channel1,int Channel2, int masterTalonId, int slaveTalonId) {
 	  Lift1 = new Solenoid(Channel1);
 	  Lift2 = new Solenoid(Channel2);
+	  
 	  masterTalon = new CANTalon(masterTalonId);
+	  masterTalon.changeControlMode(TalonControlMode.PercentVbus);
 	  slaveTalon = new CANTalon(slaveTalonId);
+	  
 	  slaveTalon.changeControlMode(TalonControlMode.Follower);
-	  slaveTalon.set(masterTalonId);		
-		
+	  slaveTalon.set(masterTalonId);				
 	}
 	
-	public void moveDropDown(boolean position){
-		Lift1.set(position);
-		Lift2.set(position);
+	public void setState(IntakeState setState){
+		if(setState == IntakeState.DOWN){
+			Lift1.set(false);
+			Lift2.set(false);
+			currentState = setState;
+		} else {
+			Lift1.set(true);
+			Lift2.set(true);
+			currentState = setState;
+		}
 	}
 	
-	
-
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-		if (){
-			switch(now){
-				case UP:
-					moveDropDown(true);
-					break;
-				case DOWN:
-					moveDropDown(false);
-					break;	
-					}
-			}
-		
+	public IntakeState getState(){
+		return currentState;
 	}
-
 	
-
+	public void setSucking(boolean isSucking){
+		if(isSucking){
+			masterTalon.set(1);
+		} else {
+			masterTalon.set(0);
+		}
+	}
 }
